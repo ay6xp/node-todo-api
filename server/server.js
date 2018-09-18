e8c16cf8b85c4393acea68f9b5fa0c2d4c7c12f9
Ahmed Youssef <ay6xp@virginia.edu>
2018-09-17 20:00:30 -0400

    added todo update

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 const {ObjectID} = require('mongodb');
-
+const _ = require('lodash');
 var app = express();
 const port = process.env.PORT || 3000;
 
@@ -71,7 +71,35 @@ app.delete('/todos/:id', (req, res) => {
 
   }).catch((e) => res.status(400).send());
 
+});
 
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body,['text','complete']);
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id,
+    {$set: body} , {new: true})
+    .then((todo) => {
+
+      if(!todo) {
+        return res.status(404).send();
+
+        }
+        res.send({todo});
+
+    })
+    .catch((e) => {
+      res.status(400).send();
+    });
 
 });
 
